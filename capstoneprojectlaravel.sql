340bdefbde6ef2b567e2d6bf4b1450433b768d5f
Maori Trixia Leonardo <maoritrixialeonardo@gmail.com>
2023-05-28 14:44:50 +0800

    Added DB File

--- capstoneprojectlaravel.sql
+++ capstoneprojectlaravel.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 27, 2023 at 07:27 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Host: localhost:8889
+-- Generation Time: May 28, 2023 at 06:44 AM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `accounts` (
-  `account_id` int(255) NOT NULL,
+  `account_id` bigint(20) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
-  `employee_number` int(255) NOT NULL,
   `default_password` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `password_changed` varchar(255) NOT NULL DEFAULT 'false',
@@ -44,12 +43,41 @@ CREATE TABLE `accounts` (
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`account_id`, `email`, `employee_number`, `default_password`, `password`, `password_changed`, `type`, `verification_code`, `first_login`, `status`) VALUES
-(1, 'admin1@adamson.edu.ph', 202011111, 'DEFAULT', NULL, 'false', 'AD', 8339, NULL, 'active'),
-(2, 'admin2@adamson.edu.ph', 202011222, 'DEFAULT', NULL, 'false', 'AD', 8632, NULL, 'active'),
-(3, 'admin3@adamson.edu.ph', 202011333, 'DEFAULT', NULL, 'false', 'AD', NULL, NULL, 'active'),
-(4, 'admin4@adamson.edu.ph', 2020114444, 'DEFAULT', NULL, 'false', 'AD', NULL, NULL, 'active'),
-(5, 'maori.trixia.leonardo@adamson.edu.ph', 202011989, 'DEFAULT', '$2y$10$IzCSE5/2BM4DFI6uOX2ifO9u.E5p9seMI/86A/ygb89/5cqcVG38K', 'false', 'PE', 1353, 'true', 'active');
+INSERT INTO `accounts` (`account_id`, `email`, `default_password`, `password`, `password_changed`, `type`, `verification_code`, `first_login`, `status`) VALUES
+(1, 'admin1@adamson.edu.ph', 'DEFAULT', NULL, 'false', 'AD', 8339, NULL, 'active'),
+(2, 'admin2@adamson.edu.ph', 'DEFAULT', NULL, 'false', 'AD', 8632, NULL, 'active'),
+(3, 'admin3@adamson.edu.ph', 'DEFAULT', NULL, 'false', 'AD', NULL, NULL, 'active'),
+(4, 'admin4@adamson.edu.ph', 'DEFAULT', NULL, 'false', 'AD', NULL, NULL, 'active'),
+(5, 'maori.trixia.leonardo@adamson.edu.ph', 'DEFAULT', '$2y$10$IzCSE5/2BM4DFI6uOX2ifO9u.E5p9seMI/86A/ygb89/5cqcVG38K', 'false', 'PE', 8841, 'true', 'deactivated');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` bigint(20) UNSIGNED NOT NULL,
+  `account_id` bigint(20) UNSIGNED NOT NULL,
+  `employee_number` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `job_title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `position` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `immediate_superior_id` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `account_id`, `employee_number`, `first_name`, `last_name`, `department_id`, `job_title`, `position`, `immediate_superior_id`) VALUES
+(1, 1, 202011111, 'Administrator', 'One', NULL, NULL, NULL, NULL),
+(2, 2, 202011222, 'Administrator', 'Two', NULL, NULL, NULL, NULL),
+(3, 3, 202011333, 'Administrator', 'Three', NULL, NULL, NULL, NULL),
+(4, 4, 202011444, 'Administrator', 'Four', NULL, NULL, NULL, NULL),
+(5, 5, 202011989, 'Maori Trixia', 'Leonardo', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -64,7 +92,7 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -88,7 +116,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2019_08_19_000000_create_failed_jobs_table', 1),
 (6, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (7, '2023_05_22_174608_modify_accounts_table', 2),
-(8, '2023_05_22_181540_modify_accounts', 3);
+(8, '2023_05_22_181540_modify_accounts', 3),
+(9, '2023_05_28_052649_drop_table_employees', 4),
+(10, '2023_05_28_052829_drop_table_employees', 5),
+(11, '2023_05_28_052927_create_employees_table', 6),
+(12, '2023_05_28_053125_add_fk_employees', 7),
+(13, '2023_05_28_055350_drop_employee_number', 8);
 
 -- --------------------------------------------------------
 
@@ -114,7 +147,7 @@ CREATE TABLE `personal_access_tokens` (
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -130,9 +163,17 @@ CREATE TABLE `personal_access_tokens` (
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`account_id`),
-  ADD UNIQUE KEY `email` (`email`,`employee_number`),
-  ADD UNIQUE KEY `accounts_email_unique` (`email`),
-  ADD UNIQUE KEY `accounts_employee_number_unique` (`employee_number`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `accounts_email_unique` (`email`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`),
+  ADD UNIQUE KEY `employees_account_id_unique` (`account_id`),
+  ADD UNIQUE KEY `employees_employee_number_unique` (`employee_number`),
+  ADD KEY `employees_immediate_superior_id_foreign` (`immediate_superior_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -169,7 +210,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `account_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employee_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -181,13 +228,24 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_account_id_foreign` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `employees_immediate_superior_id_foreign` FOREIGN KEY (`immediate_superior_id`) REFERENCES `Employees` (`employee_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
